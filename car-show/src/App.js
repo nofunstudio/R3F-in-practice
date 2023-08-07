e82fa19329466eb6ef5fbffe5916e3a340a6cea4
nofunstudio <79766302+nofunstudio@users.noreply.github.com>
2023-08-07 00:34:28 -0400

    doggos

--- car-show/src/App.js
+++ car-show/src/App.js
@@ -1,17 +1,18 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
-  EffectComposer,
-  DepthOfField,
-  Bloom,
-  ChromaticAberration,
+	EffectComposer,
+	DepthOfField,
+	Bloom,
+	ChromaticAberration,
 } from "@react-three/postprocessing";
 import { BlendFunction } from "postprocessing";
 import {
-  CubeCamera,
-  Environment,
-  OrbitControls,
-  PerspectiveCamera,
+	CubeCamera,
+	Environment,
+	OrbitControls,
+	PerspectiveCamera,
+	Clone,
 } from "@react-three/drei";
 import "./style.css";
 import { Boxes } from "./Boxes";
@@ -19,79 +20,110 @@ import { Car } from "./Car";
 import { Ground } from "./Ground";
 import { FloatingGrid } from "./FloatingGrid";
 import { Rings } from "./Rings";
+import { Model } from "./Robot";
 
 function CarShow() {
-  return (
-    <>
-      <OrbitControls 
-        target={[0, 0.35, 0]}
-        maxPolarAngle={1.45}
-      />
+	return (
+		<>
+			<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
 
-      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+			<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      <color args={[0, 0, 0]} attach="background" />
+			<color args={[0, 0, 0]} attach="background" />
 
-      <CubeCamera resolution={256} frames={Infinity}>
-        {(texture) => (
-          <>
-            <Environment map={texture} />
-            <Car />
-          </>
-        )}
-      </CubeCamera>
+			<CubeCamera resolution={256} frames={Infinity}>
+				{(texture) => (
+					<>
+						<Environment map={texture} />
+					</>
+				)}
+			</CubeCamera>
 
-      <spotLight
-        color={[1, 0.25, 0.7]}
-        intensity={1.5}
-        angle={0.6}
-        penumbra={0.5}
-        position={[5, 5, 0]}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <spotLight
-        color={[0.14, 0.5, 1]}
-        intensity={2}
-        angle={0.6}
-        penumbra={0.5}
-        position={[-5, 5, 0]}
-        castShadow
-        shadow-bias={-0.0001}
-      />
-      <Ground />
-      <FloatingGrid />
-      <Boxes />
-      <Rings />
+			<spotLight
+				color={[1, 0.25, 0.7]}
+				intensity={1.5}
+				angle={0.6}
+				penumbra={0.5}
+				position={[5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<spotLight
+				color={[0.14, 0.5, 1]}
+				intensity={2}
+				angle={0.6}
+				penumbra={0.5}
+				position={[-5, 5, 0]}
+				castShadow
+				shadow-bias={-0.0001}
+			/>
+			<Ground />
+			<FloatingGrid />
+			<Boxes />
+			<Rings />
+			<Suspense fallback={null}>
+				<Model
+					url="./doggo.glb"
+					position={[0, 0, 0]}
+					color={[1, 0, 0]}
+					speed={3.5}
+					animation="RobotDog@Walk"
+				/>
 
-      <EffectComposer>
-        {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
-        <Bloom
-          blendFunction={BlendFunction.ADD}
-          intensity={1.3} // The bloom intensity.
-          width={300} // render width
-          height={300} // render height
-          kernelSize={5} // blur kernel size
-          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
-        />
-        <ChromaticAberration
-          blendFunction={BlendFunction.NORMAL} // blend mode
-          offset={[0.0005, 0.0012]} // color offset
-        />
-      </EffectComposer>
-    </>
-  );
+				{/* Second cloned model moved to position [2, 0, 0] */}
+				<Model
+					url="./doggo2.glb"
+					position={[0.75, 0, 0]}
+					color={[0, 1, 0]}
+					speed={3}
+					animation="RobotDog@Walk"
+				/>
+
+				{/* Third cloned model moved to position [-2, 0, 0] */}
+				<Model
+					url="./doggo3.glb"
+					position={[-0.75, 0, 0]}
+					color={[0, 0, 1]}
+					speed={2}
+					animation="RobotDog@Walk"
+				/>
+				<Model
+					url="./doggo4.glb"
+					position={[1.5, -0.025, 0]}
+					color={[0.5, 0.5, 1]}
+					speed={1}
+					animation="RobotDog@Dancing"
+				/>
+			</Suspense>
+
+			<EffectComposer>
+				{/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+				<Bloom
+					blendFunction={BlendFunction.ADD}
+					intensity={1.3} // The bloom intensity.
+					width={300} // render width
+					height={300} // render height
+					kernelSize={5} // blur kernel size
+					luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+					luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+				/>
+				{/* <ChromaticAberration
+					blendFunction={BlendFunction.NORMAL} // blend mode
+					offset={[0.0005, 0.0012]} // color offset
+				/> */}
+			</EffectComposer>
+		</>
+	);
 }
 
 function App() {
-  return (
-    <Suspense fallback={null}>
-      <Canvas shadows>
-        <CarShow />
-      </Canvas>
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={null}>
+			<Canvas shadows>
+				<CarShow />
+			</Canvas>
+		</Suspense>
+	);
 }
 
 export default App;
